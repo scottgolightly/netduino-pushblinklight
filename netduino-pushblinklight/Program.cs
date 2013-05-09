c899885ffff889df769abffbe810b9e372fe7bcc
Scott Golightly <scott_golightly@hotmail.com>
2013-05-09 01:48:16 -0600

    Finished Code

---
+++ netduino-pushblinklight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace netduino_pushblinklight
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+            InputPort button = new InputPort(Pins.ONBOARD_BTN, false, Port.ResistorMode.Disabled);
+            bool buttonState = false;
+
+            while (true)
+            {
+                buttonState = button.Read();
+                led.Write(buttonState);
+            }
+
+        }
+
+    }
+}
